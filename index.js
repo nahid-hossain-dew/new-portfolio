870f35e891cbc4d65c4117c401e1cc995b62c90b
nahid-hossain-dew <149518826+nahid-hossain-dew@users.noreply.github.com>
2023-11-17 19:59:50 +0600

    created navbar hero and work section

---
+++ index.js
@@ -0,0 +1,85 @@
+// navbar section 
+let collapseButton=document.querySelector(".collapse-button");
+let collapseMenu=document.querySelectorAll(".navbar .collapse")
+console.log(collapseMenu);
+collapseButton.addEventListener("click",()=>{
+    collapseMenu.forEach((menu)=>{
+      menu.classList.toggle("showmenu");
+    })
+})
+
+
+
+
+
+
+
+
+
+// fade
+let fadeEl = document.querySelector(".fade");
+let scrolled = false;
+window.addEventListener("scroll", () => {
+    if (!scrolled) {
+
+        fadeEl.style.opacity = "0.3";
+        scrolled = true;
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// filter
+let gridEl = document.querySelector(".grid");
+console.log(gridEl);
+let filterItems = document.querySelector(".filter-items");
+let projects = document.querySelectorAll(".project");
+console.log(projects);
+let technologies = document.querySelectorAll(".technology");
+console.log(technologies.forEach(element => {
+    console.log(element.innerText);
+}));
+
+
+
+
+
+filterItems.addEventListener("click", (e) => {
+    // remove fade
+    fadeEl.style.opacity = "0";
+    // added active class
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    // reorganize grid
+    console.log(e.target.innerText);
+    if (e.target.innerText === "ALL") {
+        projects.forEach((project) => {
+            project.style.display = "block";
+        })
+    } else if (e.target.innerText === "JAVA SCRIPT") {
+        projects[0].style.display = "block";
+        projects[2].style.display = "block";
+        projects[4].style.display = "block";
+        projects[1].style.display = "none";
+        projects[3].style.display = "none";
+        projects[5].style.display = "none";
+    } else if (e.target.innerText === "HTML & CSS") {
+        projects[1].style.display = "block";
+        projects[3].style.display = "block";
+        projects[5].style.display = "block";
+        projects[0].style.display = "none";
+        projects[2].style.display = "none";
+        projects[4].style.display = "none";
+    }
+
+})
